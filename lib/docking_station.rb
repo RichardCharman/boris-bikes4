2e550e67f387056e952b32e96e34460ec787e2be
Richard Charman <richardcharman@live.com>
2015-07-22 20:46:28 +0000

    Introduced private helper methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,14 +3,25 @@ class DockingStation
   def initialize
     @bikes = []
   end
-  
-  def dock bike
-    fail 'Docking station full' if @bikes.count >= 20
-    @bikes << bike
-  end
 
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
+  
+  def dock bike
+    fail 'Docking station full' if full?
+    @bikes << bike
+  end
+  
+  private
+  
+  def full?
+    @bikes.count >= 20
+  end
+  
+  def empty?
+    @bikes.empty?
+  end   
+  
 end
