f5afe7de2f3498a40db4b748df8876eee541318d
Josh <Tattsy@hotmail.co.uk>
2015-07-23 16:43:20 +0100

    added bike container module and tests

---
+++ lib/bike_container.rb
@@ -0,0 +1,29 @@
+module BikeContainer
+  
+  DEFAULT_CAPACITY = 20
+
+  attr_reader :capacity
+
+  def initialize(capacity= DEFAULT_CAPACITY)
+  	@bikes = []
+  	@capacity = capacity
+  end
+
+  def add_bike(bike)
+  	raise "#{self.class.name} full" if full?
+  	bikes << bike
+  end
+
+  def empty?
+  	bikes.empty?
+  end
+
+  def full?
+  	bikes.count >= capacity
+  end
+
+  private
+
+  attr_reader :bikes
+  
+end
